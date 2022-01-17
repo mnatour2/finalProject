391204bd494767678c5aa9cd7fc64fc262c455af
Natour <mohammad.alnatour00@hotmail.com>
2022-01-17 22:28:15 +0200

    fix login bugs

--- app/src/main/java/com/example/finalproject/Login.java
+++ app/src/main/java/com/example/finalproject/Login.java
@@ -13,13 +13,10 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -30,7 +27,6 @@ public class Login extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private RequestQueue queue;
-    boolean valid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,68 +38,57 @@ public class Login extends AppCompatActivity {
     }
 
     public void onLogin(View view) {
-
         String enteredUsername = username.getText().toString();
         String enteredPassword = password.getText().toString();
-
         if (enteredPassword.contains("admin") && enteredUsername.contains("admin")) {
             Intent homeReceptionist = new Intent(getApplicationContext(), HomeReceptionist.class);
             startActivity(homeReceptionist);
         } else {
-
             isValid(enteredUsername, enteredPassword);
-            if (valid) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-                SharedPreferences.Editor editor = prefs.edit();
-                Gson gson = new Gson();
-                String gsonName = gson.toJson(enteredUsername);
-                editor.putString("username", gsonName);
-                editor.commit();
-
-                Intent homeCustomer = new Intent(getApplicationContext(), HomeCustomer.class);
-                startActivity(homeCustomer);
-            }
         }
-
     }
 
     public void isValid(String username, String password) {
-
-        String url = "http://192.168.0.102/android/getCustomers.php";
+        String url = "http://192.168.1.138/android/getCustomers.php";
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,
-                null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                ArrayList<String> userNames = new ArrayList<>();
-                ArrayList<String> passwords = new ArrayList<>();
-                for (int i = 0; i < response.length(); i++) {
+                null, response -> {
+
+            ArrayList<JSONObject> users = new ArrayList<>();
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    users.add(response.getJSONObject(i));
+                } catch (JSONException exception) {
+                    Log.d("Error", exception.toString());
+                }
+            }
+
+            if (!password.isEmpty() && !username.isEmpty()) {
+                for (int i = 0; i < users.size(); i++) {
                     try {
-                        JSONObject obj = response.getJSONObject(i);
-                        userNames.add(obj.getString("Username"));
-                        passwords.add(obj.getString("Pass"));
-                    } catch (JSONException exception) {
-                        Log.d("Error", exception.toString());
+                        if (username.equals(users.get(i).getString("Username")) && password.equals(users.get(i).getString("Pass"))) {
+                            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                            SharedPreferences.Editor editor = prefs.edit();
+                            Gson gson = new Gson();
+                            String gsonName = gson.toJson(username);
+                            editor.putString("username", gsonName);
+                            editor.commit();
+                            System.out.println(gsonName);
+                            Intent homeCustomer = new Intent(getApplicationContext(), HomeCustomer.class);
+                            startActivity(homeCustomer);
+                            return;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
                 }
-
-                for (int i = 0; i < userNames.size(); i++) {
-                    if ((userNames.get(i).contains(username)) && (passwords.get(i).contains(password))) {
-                        valid = true;
-                        return;
-                    }
-                }
-                Toast.makeText(Login.this, "No such customer, Please register.",
+                Toast.makeText(Login.this, "Wrong Password or Username.",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(Login.this, "Please fill in the data.",
                         Toast.LENGTH_SHORT).show();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                Toast.makeText(Login.this, error.toString(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        }, error -> Toast.makeText(Login.this, error.toString(),
+                Toast.LENGTH_SHORT).show());
         queue.add(request);
     }
 
